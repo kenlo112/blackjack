12067ef5f4a1b0d995d3f131dc84f3bc47db32dd
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-01-04 18:12:21 -0500

    update getRamdom function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 const suits = ["♠", "♥", "♣", "♦"]
+let dealer = []
+let player = []
 const hit = document.getElementById("hit")
 const stand = document.getElementById("stand")
 const restart = document.getElementById("restart")
+let deck = []
 
-  function createNewDeck() {
-    const deck = []
+function createNewDeck() {    
     values.forEach(value => {
         suits.forEach(suit => {
             deck.push(value + suit)
@@ -14,7 +16,10 @@ const restart = document.getElementById("restart")
     return deck
   }
 
-  console.log(createNewDeck());
-
-
+function getRandomCard() {
+    const idx = Math.floor(Math.random() * deck.length)
+    const card = deck[idx]
+    deck.splice(idx, 1)
+    return card
+  }
   
