b0d5020ed02bb7e2bd2c1113ba710386e42b1cb6
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-01-07 18:30:41 -0500

    a

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ const suits = ["♠", "♥", "♣", "♦"]
 let dealer = []
 let player = []
 let deck = []
-let playerHand = []
+var playerHand = []
 
 const deal = document.getElementById("deal")
 deal.addEventListener("click", firstHand)
@@ -33,7 +33,6 @@ function getRandomCard() {
     return card
   }
 
-// take a look later
 const dealerHandEl = document.createElement("div")
 dealerHandEl.classList.add("cards")
 dealerCardContainer.appendChild(dealerHandEl)
@@ -43,36 +42,58 @@ playerHandEl.classList.add("cards")
 playerCardContainer.appendChild(playerHandEl)
 
 function firstHand() {
-  dealer = [getRandomCard(), getRandomCard()];
-  dealer.forEach(card => {
-      const cardEl = document.createElement("div")
-      cardEl.classList.add("card")
-      cardEl.innerText = card
-      dealerHandEl.appendChild(cardEl)
-  })
-  player = [getRandomCard(), getRandomCard()];
-  player.forEach(card => {
-      const cardEl = document.createElement("div")
-      cardEl.classList.add("card")
-      cardEl.innerText = card
-      playerHandEl.appendChild(cardEl)
-  })
-}
+    const dealerHand = [getRandomCard(), getRandomCard()];
+    dealerHand.forEach(card => {
+        const cardEl = document.createElement("div")
+        cardEl.classList.add("card")
+        cardEl.innerText = card
+        dealerHandEl.appendChild(cardEl)
+    })
+    const playerHand_first = [getRandomCard(), getRandomCard()];
+    playerHand.push(playerHand_first[0], playerHand_first[1])
+    playerHand_first.forEach(card => {
+        const cardEl = document.createElement("div")
+        cardEl.classList.add("card")
+        cardEl.innerText = card
+        playerHandEl.appendChild(cardEl)
+    })
+  }
 
 function disableEventListener() {
     deal.removeEventListener("click", firstHand)
   }
 deal.addEventListener("click", disableEventListener)
 
+function calVal(hand){
+  let value = 0
+  let ace_count = 0
+  hand.forEach(card => {
+    if (card[0] == "J" || card[0] == "Q" || card[0] == "K") {
+      value += 10
+    } else if (card[0] == "A") {
+      value += 1
+      ace_count += 1
+    } else if (card == "10") {
+      value += parseInt(card)
+    } else {
+      value += parseInt(card[0])
+    }
+  })
+
+  let result = [value]
+  for (let index = 0; index < ace_count; index++) {
+    result.push(result[result.length-1] + 10)
+  }
+
+  return result
+}
+
 function playerHit() {
     const newCard = getRandomCard()
-    player.push(newCard)
+    playerHand.push(newCard)
     const cardEl = document.createElement("div");
     cardEl.classList.add("card");
     cardEl.innerText = newCard;
     playerHandEl.appendChild(cardEl);
-    //delete later
-    console.log(player)
+    console.log(calVal(playerHand))     
   }
-
-
